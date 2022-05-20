2a0d9306ae5cd0742dccb6391b35376a6cf870b4
Stephen Reish <spreish@ncsu.edu>
2022-05-20 19:59:51 -0400

    Forgot to push!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { bookData } from './book-data';
 
 function App() {
+  const [books] = useState(bookData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {books.map((book) => (
+        <div className="bookElement">
+          <div className="allDesc">
+            <div className="shortDesc">
+              <h1>{book.title}</h1>
+              <p>{book.author}</p>
+              <p>{book.shortDescription}</p>
+            </div>
+            <div className="longDesc">
+              <p>Wrap the below data in a dropdown:</p>
+              <div className="dropdown">
+                <div>
+                  <p>URL: <a href={book.coverImageUrl}>{book.coverImageUrl}</a></p>
+                </div>
+                <p>Publisher: {book.publisher}</p>
+                <p>Date Published: {book.publicationDate}</p>
+                <p>Full Description:<br></br><br></br>
+                  {book.detailedDescription}</p>
+              </div>
+            </div>
+          </div>
+          <div className="bookImageContainer">
+            <img className="bookImage" src={book.coverImageUrl} alt={book.title}></img>
+          </div>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
